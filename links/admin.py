b8180e029c24179a639744407fa0aee3c5db5147
Даниил Тоненьков <dtonenkov@mail.ru>
2024-06-16 03:22:56 +0300

    Создать админку, мигрировать бд

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Group, Resource, Tag
+
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "author",
+        "category",
+        "group",
+        "url",
+        "pub_datetime",
+        "done_datetime",
+    )
+
+    search_fields = (
+        "title",
+        "url",
+    )
+
+    list_filter = ("pub_datetime", "done_datetime")
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "author",
+        "pub_datetime",
+        "parent",
+        "is_publish",
+    )
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "slug",
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "slug",
+    )
--- links/models.py
+++ links/models.py
@@ -1,16 +1,23 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.db import models
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=80)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_publish = models.BooleanField(default=True)
+    title = models.CharField(verbose_name="Заголовок группы", max_length=80)
+    author = models.ForeignKey(
+        User, verbose_name="Автор группы", on_delete=models.CASCADE
+    )
+    is_publish = models.BooleanField("Статус выполнения", default=True)
     pub_datetime = models.DateTimeField("Дата публикации", auto_now_add=True)
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
+    parent = models.ForeignKey(
+        "self", verbose_name="Родитель", on_delete=models.CASCADE, blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Группа"
+        verbose_name_plural = "Группы"
 
     def __str__(self):
         return self.title
@@ -19,12 +26,20 @@ class Group(models.Model):
 class Tag(models.Model):
     slug = models.SlugField()
 
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+
     def __str__(self):
-        return self.title
+        return self.slug
 
 
 class Category(models.Model):
     slug = models.CharField(max_length=20)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.slug
@@ -32,40 +47,31 @@ class Category(models.Model):
 
 class Resource(models.Model):
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name="Автор", on_delete=models.CASCADE)
 
-    title = models.CharField(max_length=80)
+    title = models.CharField(verbose_name="Заголовок", max_length=80)
 
     url = models.URLField("Адрес ресурса")
 
     pub_datetime = models.DateTimeField("Дата публикации", auto_now_add=True)
 
-    done_datetime = models.DateTimeField("Дата выполнения")
+    done_datetime = models.DateTimeField("Дата выполнения", blank=True, null=True)
 
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE,
+        Category,
+        on_delete=models.CASCADE,
     )
 
-    Tag = models.ManyToManyField(
-        Tag, blank=True, null=True
-    )
+    Tag = models.ManyToManyField(Tag, blank=True, null=True)
 
     class Meta:
         ordering = ["-pub_datetime"]
         verbose_name = "Ресурс"
-        verbose_name = "Ресурсы"
+        verbose_name_plural = "Ресурсы"
         default_related_name = "resources"
         default_related_name = "resources"
 
     def __str__(self):
-        return f'{self.title}'
-
-
-class TagResource(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.tag} {self.resource}'
+        return f"{self.title}"
